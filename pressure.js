eab784f1eb67acadf1cc5d5c2935298c21b8ed4a
lucasmcazelli <63100230+lucasmcazelli@users.noreply.github.com>
2023-08-11 14:53:59 +0100

    Add rate of rise - fix canva

--- pressure.js
+++ pressure.js
@@ -1,156 +1,104 @@
-console.log('Lucas Mischiatti Cazelli');
-console.log('Calculadora de pressao de concreto');
+const g = 9.81; // Acceleration due to gravity in m/s^2
+const concreteSetTimeHours = 2; // Assuming concrete starts setting after 2 hours
 
+let currentHeight = 0; // To keep track of the current height during animation
 
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
+function calculateAndDraw() {
+    // Retrieve input values
+    const formworkHeight = parseFloat(document.getElementById("formworkHeight").value) / 100; // Convert to meters
+    const concreteHeight = parseFloat(document.getElementById("concreteHeight").value) / 100; // Convert to meters
+    const concreteWeight = parseFloat(document.getElementById("concreteWeight").value);
+    const formworkResistance = parseFloat(document.getElementById("formworkResistance").value);
+    const rateOfRise = parseFloat(document.getElementById("rateOfRise").value) / 100; // Convert to meters/hour
 
+    // Calculate the effective fluid height
+    const fillTimeHours = concreteHeight / rateOfRise;
+    const fluidTime = fillTimeHours < concreteSetTimeHours ? fillTimeHours : concreteSetTimeHours;
+    const effectiveFluidHeight = rateOfRise * fluidTime;
 
+    const pMax = concreteWeight * g * effectiveFluidHeight;
 
-//
-//
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
 
+    // Clear previous drawings
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-//fundo
-const fundo ={
-    desenha(){
-      ctx.fillStyle = '#70c5ce';
-      ctx.fillRect(0,0, c.width, c.height-50)
-  }
+    // Draw formwork
+    drawFormwork();
+
+    // Start animation
+    currentHeight = 0;
+    requestAnimationFrame(() => animatePressure(ctx, concreteHeight, formworkHeight, pMax, formworkResistance, rateOfRise));
 }
 
-
-//chao
-const chao ={
-  espessura: 50,
-  desenha(){
-    ctx.fillStyle = '#900C3F';
-    ctx.fillRect(0,c.height-chao.espessura, c.width, c.height)
-  }
+function drawFormwork() {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#aaa";
+    ctx.fillRect(50, canvas.height - 400, 100, 400); // Assuming a max canvas height of 400 for visualization
 }
 
+function animatePressure(ctx, concreteHeight, formworkHeight, pMax, formworkResistance, rateOfRise) {
+    // Increase the currentHeight based on rateOfRise
+    currentHeight += rateOfRise / 60; // Divided by 60 for 60 frames per second approximation
 
-                 //  pass the number of iterations as an argument
+    // Calculate current pressure
+    const currentPressure = currentHeight * g * rateOfRise;
 
+    // Clear previous triangle/trapezoid
+    ctx.clearRect(50, canvas.height - 400, 150, 400);
 
-const forma =
-{
-  altura: 250,
-  largura: 10,
-  desenha(){
-    ctx.fillStyle = 'black';
-    ctx.fillRect(
-      (c.width/2) - forma.largura,
-      c.height - chao.espessura - forma.altura,
-      forma.largura,
-      forma.altura,
-    )
-  }
+    // Redraw formwork
+    drawFormwork();
 
-};
+    if (currentPressure <= formworkResistance) {
+        // Draw regular triangle
+        ctx.fillStyle = "rgba(255,0,0,0.5)";
+        ctx.beginPath();
+        ctx.moveTo(50, canvas.height);
+        ctx.lineTo(50, canvas.height - (currentHeight * 200));
+        ctx.lineTo(50 + (currentPressure / 10), canvas.height);
+        ctx.closePath();
+        ctx.fill();
+    } else {
+        // Draw trapezoid up to resistance and dashed triangle for the rest
 
-
-
-
-
-//concreto
-const concreto = {
-  h_maxconcreto: 250,
-  h_concreto: 0,
-  densidade: 0.25,
-  pmax: 10,
-  pressaolimitada: 35,
-  pressaoatual: 0,
-  h_atual: 0,
-
-    desenha()
-    {
-      var p = concreto.h_concreto * concreto.densidade;
-      var h = 0;
-      //hydro
-      ctx.fillStyle = 'gray';
-      ctx.beginPath();
-      ctx.moveTo((c.width/2), (c.height- chao.espessura - concreto.h_concreto));
-      ctx.lineTo(((c.width/2) + p), c.height- chao.espessura);
-      ctx.lineTo((c.width/2), c.height- chao.espessura);
-      ctx.closePath();
-      ctx.stroke()
-
-      //limitado
-      ctx.fillStyle = 'gray';
-      ctx.beginPath();
-      ctx.moveTo((c.width/2), (c.height- chao.espessura - concreto.h_concreto));
-      if(p>concreto.pressaolimitada)
-      {
-        p = concreto.pressaolimitada;
-        ctx.lineTo(((c.width/2) + p), c.height - chao.espessura - concreto.h_concreto + (concreto.pressaolimitada/concreto.densidade));
-        
-      };
-      ctx.lineTo(((c.width/2) + p), c.height- chao.espessura);
-      ctx.lineTo((c.width/2), c.height- chao.espessura);
-      ctx.closePath();
-      ctx.fill()
-      
-      },
-      
-
-    concreta()
-    {
-          if( concreto.h_concreto < concreto.h_maxconcreto ){
-            setTimeout(concreto.concreta, 100000000);
-            concreto.h_concreto++;
-            
-           } else {
-             concreto.encheu();
-           }
-
-    },
-    encheu(){break}
-  };
-
-
-//tirantes
-const tirante = {
-  altura: 350,
-  diametro: 15,
-
-
-}
-
+    // Calculate where the trapezoid's top will be based on the formwork's resistance
+    const trapezoidTop = canvas.height - (formworkResistance / (rateOfRise * g) * 200);
+    const trapezoidBaseWidth = formworkResistance / 10;
     
-
-
-
-
-//main
-function mostra(hf,hc,pm) {
-  document.getElementById("hf").innerHTML =  forma.altura;
-  document.getElementById("hc").innerHTML =  concreto.h_maxconcreto;
-  document.getElementById("pm").innerHTML =  concreto.pressaolimitada;
-  concreto.h_concreto = 0
-  loop()
-}
-function loop() {
-  fundo.desenha();
-  chao.desenha();
-  forma.desenha();
-  concreto.desenha();
-  concreto.concreta();
+    // Draw trapezoid
+    ctx.fillStyle = "rgba(255,0,0,0.5)";
+    ctx.beginPath();
+    ctx.moveTo(50, canvas.height);
+    ctx.lineTo(50, trapezoidTop);
+    ctx.lineTo(50 + trapezoidBaseWidth, trapezoidTop);
+    ctx.lineTo(50 + (currentPressure / 10), canvas.height);
+    ctx.closePath();
+    ctx.fill();
     
+    // Draw dashed triangle for the rest
+    const triangleTop = canvas.height - (currentHeight * 200);
+    
+    ctx.fillStyle = "rgba(255,100,100,0.3)"; // Lighter fill for the triangle
+    ctx.setLineDash([5, 5]); // Set dashed lines
+    ctx.beginPath();
+    ctx.moveTo(50, trapezoidTop);
+    ctx.lineTo(50, triangleTop);
+    ctx.lineTo(50 + trapezoidBaseWidth, trapezoidTop);
+    ctx.closePath();
+    ctx.fill();
+    ctx.strokeStyle = "rgba(255,0,0,0.5)";
+    ctx.stroke();
+    ctx.setLineDash([]); // Reset line style for other drawings
 
 
 
+    }
 
-
-  requestAnimationFrame(loop);
-  }
-  
-
-
-
-
-//window.addEventListener('click', function() {
-// 
-// });
-//
-
+    if (currentHeight <= concreteHeight) {
+        // Continue animation
+        requestAnimationFrame(() => animatePressure(ctx, concreteHeight, formworkHeight, pMax, formworkResistance, rateOfRise));
+    }
+}
